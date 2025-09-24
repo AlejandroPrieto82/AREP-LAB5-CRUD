5b43c19d3ccf64ded5a077cadfa0af2b1539a5f4
Alejandro Prieto <samuel.prieto-r@mail.escuelaing.edu.co>
2025-09-24 08:16:53 -0500

    Agregando base

---
+++ src/test/java/eci/edu/arep/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.arep.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
